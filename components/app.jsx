000392644fd208868838dd59c43bd04ba3b2abe7
Bo <pangb86@gmail.com>
2018-03-24 00:05:09 -0700

    Add banner component

--- components/app.jsx
+++ components/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+// components
 import Navbar from './navbar';
+import Banner from './banner';
 
 const App = () => (
   <div className="main-app-container">
     <Navbar/>
+    <Banner/>
   </div>
 );
 
---
+++ components/banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <div className="banner-main">
+        <div className="banner-info">
+          <div>
+            We'd like to get to know you better. Tell us about yourself
+            and the sort of someone you'd like to meet. Don't worry, no
+            one will see this besides the matchmakers at Tawkify.
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Banner;
