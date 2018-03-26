a218d6f6277d1d2163e4d20c0cb6c5232a11c9a6
Bo <pangb86@gmail.com>
2018-03-25 22:38:38 -0700

    Add input fields

--- components/about_form.jsx
+++ components/about_form.jsx
@@ -5,6 +5,15 @@ import StepsIndicator from './steps_indicator';
 class AboutForm extends React.Component {
   constructor(){
     super();
+    this.state = {
+      gender:"",
+      seeking:"",
+      location:"",
+    };
+  }
+
+  update(field) {
+    return e => this.setState({[field]: e.currentTarget.value});
   }
 
   render() {
@@ -17,17 +26,43 @@ class AboutForm extends React.Component {
         <div className="about-message">
           {this.props.message}
         </div>
-        <div classNam="about-input-main">
+        <div className="about-input-main">
           <div className="about-input-1">
-            <div className="about-gender">
-              <span>Gender</span>
-              <select>
+            <div className="about-group">
+              <span className="about-bold">YOUR GENDER</span>
+              <select
+                className="about-select"
+                onChange={this.update("gender")}>
+                <option style={{display: "none"}} selected value=""></option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
               </select>
+              <span class="about-small">Select your gender.</span>
+            </div>
+            <div className="about-group">
+              <span className="about-bold">LOCATION</span>
+              <input
+                className="about-input"
+                placeholder="Zipcode"
+                type="text"
+                onChange={this.update("location")}
+                value={this.state.location}/>
+              <span className="about-small">Where are you located?</span>
             </div>
           </div>
           <div className="about-input-2">
+            <div className="about-group">
+              <span className="about-bold">YOU ARE SEEKING</span>
+              <select
+                className="about-select"
+                onChange={this.update("seeking")}>
+                <option style={{display: "none"}} selected value=""></option>
+                <option value="men">Men</option>
+                <option value="women">Women</option>
+                <option value="both">Both</option>
+              </select>
+              <span class="about-small">Select the sexual orientation you're seeking.</span>
+            </div>
           </div>
         </div>
       </div>
